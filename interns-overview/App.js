85e4e5ae35316a6e6e8c9b7f4352c351f1bffa09
Yoran de Quay <yoranq@hotmail.com>
2021-02-08 15:30:06 +0100

    every second element is now different colour

--- interns-overview/App.js
+++ interns-overview/App.js
@@ -1,23 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { Text, ActivityIndicator, View, Image, FlatList, StyleSheet, RefreshControl} from 'react-native';
+import React, { useState, useEffect } from "react";
 
-export default function App() {
+export default function indexPage() {
+  const [loading, setLoading] = useState(true)
+  const [internArray, setInternArray] = useState([])
+
+  useEffect(() => {
+    fetch("http://192.168.0.106/PERIODE%2011/stage/show-yourself-backend/api/getInterns.php")
+    .then((response) => response.json())
+      .then((json) => setInternArray(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  function isOdd(num) { 
+    evenOrUneven = num % 2;
+    if (evenOrUneven = 0) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+
+
+
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("./assets/logo.png")}
-      />
-    
-      <StatusBar style="auto" />
+    <View>
+      <Image source={require("./assets/logo.png")} />
+      {loading ? <ActivityIndicator/> : (
+        <FlatList
+          data={internArray}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+          console.log(isOdd(item.id)),
+          <Text style={[styles.text, (isOdd(item.id) == 0) ? styles.itemEven : styles.itemUneven]}>{item.name}, {item.category}</Text>
+          )}
+        />
+      )}
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  container: {
+  itemEven: {
+    backgroundColor: '#154273',
+    color: '#FFF',
+    padding: 20,
+  },
+  itemUneven: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#FFF',
+    color: '#000000',
+    padding: 20,
+  },
+  text: {
+    fontSize: 20,
   },
 });
