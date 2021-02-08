ba039412a0ff0bd31680cca13292e34931df8ec7
Yoran de Quay <yoranq@hotmail.com>
2021-02-08 10:47:06 +0100

    added logo

--- interns-overview/App.js
+++ interns-overview/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require("./assets/logo.png")}
+      />
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +19,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
