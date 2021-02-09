b0162669bfa969da7792b05d671c081f1f1684bd
Yoran de Quay <yoranq@hotmail.com>
2021-02-09 13:09:06 +0100

    added searchbar and searchbar functionality

--- interns-overview/App.js
+++ interns-overview/App.js
@@ -1,37 +1,100 @@
-import { Text, ActivityIndicator, View, Image, FlatList, StyleSheet, RefreshControl} from 'react-native';
+import { Text, ActivityIndicator, View, Image, FlatList, StyleSheet, RefreshControl, TextInput} from 'react-native';
 import React, { useState, useEffect } from "react";
+import filter from 'lodash.filter';
 
 export default function indexPage() {
-  const [loading, setLoading] = useState(true)
-  const [internArray, setInternArray] = useState([])
+  const API_ENDPOINT = `http://192.168.0.106/PERIODE%2011/stage/show-yourself-backend/api/getInterns.php`;
+  const [loading, setLoading] = useState(true);
+  const [internArray, setInternArray] = useState([]);
+  const [fullInternArray, setFullInternArray] = useState([]);
+  const [query, setQuery] = useState('');
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  renderHeader = () => 
+  (
+    <View
+      style={styles.border}
+    >
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        clearButtonMode="always"
+        value={query}
+        onChangeText={queryText => handleSearch(queryText)}
+        placeholder="Search"
+      />
+    </View>
+  )
+
   useEffect(() => {
-    fetch("http://192.168.0.106/PERIODE%2011/stage/show-yourself-backend/api/getInterns.php")
+    fetch(API_ENDPOINT)
     .then((response) => response.json())
-      .then((json) => setInternArray(json))
+      .then((json) => setInternArray(json) & setFullInternArray(json))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
 
+  const onRefresh = React.useCallback(async() => {
+    setRefreshing(true);
+    let response = await fetch(
+      'http://192.168.0.106/PERIODE%2011/stage/show-yourself-backend/api/getInterns.php',
+    );
+    let responseJson = await response.json();
+    setInternArray(responseJson);
+    setRefreshing(false);
+  }, []);
+
   function isOddNumber(num) { 
-    evenOrUneven = num % 2;
+    this.evenOrUneven = num % 2;
     if (evenOrUneven == 0) {
-      console.log("even");
       return 0;
     } else {
-      console.log("odd");
       return 1;
     }
   }
 
+  const handleSearch = text => {
+    const formattedQuery = text;
+    const filteredData = filter(internArray, intern => {
+      return contains(intern, formattedQuery);
+    });
+    setFullInternArray(filteredData);
+    setQuery(text);
+  };
+  
+  const contains = ({ name }, query) => {
+    const fullName = name;
+    if (fullName.includes(query)) {
+      return true;
+    }
+    return false;
+  };
+
+  
+  
   return (
     <View>
       <Image source={require("./assets/logo.png")} />
       {loading ? <ActivityIndicator/> : (
         <FlatList
-          data={internArray}
+        ListHeaderComponent={this.renderHeader}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+            />
+          }
+          contentContainerStyle={{ paddingBottom: 144 }}
+          style={styles.flatList}
+          data={fullInternArray}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
-          <Text style={styles.text, (isOddNumber(item.id) == 0) ? styles.itemUneven : styles.itemEven}>{item.name}, {item.category}</Text>
+          <Text style={(isOddNumber(item.id) == 0) ? styles.itemUneven : styles.itemEven}>
+            {item.id} {"\n"}
+            {item.name}, {item.category}{"\n"}
+            {item.begin_internship} till {item.end_internship}{"\n"}
+            Groep: {item.groupID}
+            </Text>
           )}
         />
       )}
@@ -43,14 +106,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#154273',
     color: '#FFF',
     padding: 20,
+    fontSize: 20,
   },
   itemUneven: {
     flex: 1,
     backgroundColor: '#FFF',
     color: '#000000',
     padding: 20,
-  },
-  text: {
     fontSize: 20,
   },
+  flatList: {
+    flexGrow: 1,
+  },
+  border: {
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 20,
+  }
 });
